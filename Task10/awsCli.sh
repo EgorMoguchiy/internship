ceb32e9eac4e35278aa970750c97c054c02c29ae
Egor <emoguchiy@list.ru>
2023-01-17 12:48:07 +0300

    Python + Bash + EC2

--- Task10/awsCli.sh
+++ Task10/awsCli.sh
@@ -8,18 +8,16 @@ then
     then
         echo 'Linux Ubuntu'
         sudo apt install awscli
-        sudo mkdir ~/.aws
-        sudo aws configure set profile.default.region $REGION
-        sudo aws configure set profile.default.aws_access_key_id $ACCESS_PUBLIC
-        sudo aws configure set profile.default.aws_secret_access_key $ACCESS_SECRET
+        aws configure set profile.default.region $REGION
+        aws configure set profile.default.aws_access_key_id $ACCESS_PUBLIC
+        aws configure set profile.default.aws_secret_access_key $ACCESS_SECRET
     elif [ -f /etc/redhat-release ]
     then
         echo 'Linux Redhat'
         sudo dnf install awscli
-        sudo mkdir ~/.aws
-        sudo aws configure set profile.default.region $REGION
-        sudo aws configure set profile.default.aws_access_key_id $ACCESS_PUBLIC
-        sudo aws configure set profile.default.aws_secret_access_key $ACCESS_SECRET
+        aws configure set profile.default.region $REGION
+        aws configure set profile.default.aws_access_key_id $ACCESS_PUBLIC
+        aws configure set profile.default.aws_secret_access_key $ACCESS_SECRET
     fi
 elif [ "$OSTYPE" == "darwin" ]
 then
@@ -27,10 +25,9 @@ then
     echo 'Mac OSX'
     curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
     sudo installer -pkg AWSCLIV2.pkg -target /
-    sudo mkdir ~/.aws
-    sudo aws configure set profile.default.region $REGION
-    sudo aws configure set profile.default.aws_access_key_id $ACCESS_PUBLIC
-    sudo aws configure set profile.default.aws_secret_access_key $ACCESS_SECRET
+    aws configure set profile.default.region $REGION
+    aws configure set profile.default.aws_access_key_id $ACCESS_PUBLIC
+    aws configure set profile.default.aws_secret_access_key $ACCESS_SECRET
 elif [ "$OSTYPE" == "msys" ]
 then
     # Windows
